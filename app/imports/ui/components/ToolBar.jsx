d8850249f4d9dfcd457b8ca7bbb1ddcaf0b328f9
Jamse Galway-Sevetson <jtmoka@hawaii.edu>
2021-01-31 13:26:12 -1000

    Update ToolBar.jsx

--- app/imports/ui/components/ToolBar.jsx
+++ app/imports/ui/components/ToolBar.jsx
@@ -3,15 +3,20 @@ import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import { withRouter, NavLink } from 'react-router-dom';
-import { Menu, Dropdown, Header } from 'semantic-ui-react';
+import { Menu, Dropdown, Header, Segment, Statistic } from 'semantic-ui-react';
 import { Roles } from 'meteor/alanning:roles';
 
 
 /** The ToolBar appears at the top of every page. Rendered by the App Layout component. */
 class ToolBar extends React.Component {
   render() {
-    const menuStyle = { marginBottom: '10px' };
+
+
+
+    const menuStyle = { marginBottom: '10px', background: '#001947' };
     return (
+
+
       <Menu style={menuStyle} attached = "top" borderless inverted>
         <Menu.Item as={NavLink} activeClassName="" exact to="/">
           <Header inverted as = 'h1'>HEI</Header>
@@ -48,6 +53,7 @@ class ToolBar extends React.Component {
           }
         </Menu.Item>
       </Menu>
+
     );
   }
 }
