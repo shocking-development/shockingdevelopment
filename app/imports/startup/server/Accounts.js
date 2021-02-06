2153cf440b19195449828abf12b1d94357bee98c
Shun <47809066+saitoshi@users.noreply.github.com>
2021-02-05 18:37:36 -1000

    issue-06 February 5

--- app/imports/startup/server/Accounts.js
+++ app/imports/startup/server/Accounts.js
@@ -4,7 +4,7 @@ import { Roles } from 'meteor/alanning:roles';
 
 /* eslint-disable no-console */
 
-function createUser(email, password, role) {
+function createUser(email, password, role, firstName, lastName, zipcode, transportation) {
   console.log(`  Creating user ${email}.`);
   const userID = Accounts.createUser({
     username: email,
@@ -21,7 +21,12 @@ function createUser(email, password, role) {
 if (Meteor.users.find().count() === 0) {
   if (Meteor.settings.defaultAccounts) {
     console.log('Creating the default user(s)');
-    Meteor.settings.defaultAccounts.map(({ email, password, role }) => createUser(email, password, role));
+    Meteor.settings.defaultAccounts.map(({ email, password, role, firstName,
+        lastName, zipcode, transportation,
+    }) => {
+      createUser(email, password, role, firstName,
+          lastName, zipcode, transportation,);
+    });
   } else {
     console.log('Cannot initialize the database!  Please invoke meteor with a settings file.');
   }
