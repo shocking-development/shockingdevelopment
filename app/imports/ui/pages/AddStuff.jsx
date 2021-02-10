58b4937656e8f56c5ea7a245bc9f7d5e11bd2fde
awongway <awong20@hawaii.edu>
2021-02-10 12:49:43 -1000

    Update AddStuff.jsx

--- app/imports/ui/pages/AddStuff.jsx
+++ app/imports/ui/pages/AddStuff.jsx
@@ -44,7 +44,7 @@ class AddStuff extends React.Component {
     return (
         <Grid container centered>
           <Grid.Column>
-            <Header as="h2" textAlign="center">Add Stuff</Header>
+            <Header as="h2" textAlign="center">Adding a new user</Header>
             <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => this.submit(data, fRef)} >
               <Segment>
                 <TextField name='name'/>
