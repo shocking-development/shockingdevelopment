689447f564210b4cfaffd500e15d7c90fe5fe3b0
trey-sumida <tasumida@hawaii.edu>
2021-02-10 21:15:21 -1000

    Converted to function components

--- app/imports/ui/components/EditProfileForm.jsx
+++ app/imports/ui/components/EditProfileForm.jsx
@@ -1,71 +1,70 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Container, Grid, Button } from 'semantic-ui-react';
 
-class EditProfileForm extends Component {
-    render() {
-        return (
-            <div>
-                <Container>
-                    <Grid stackable columns={2} style={{ paddingLeft: '20px', paddingRight: '20px', paddingBottom: '20px' }}>
-                        <Grid.Row>
-                            <Grid.Column width={4} className="edit-profile-col">
-                                NAME
-                            </Grid.Column>
-                            <Grid.Column>
-                                <input className="edit-profile-input" type="text" />
-                            </Grid.Column>
-                        </Grid.Row>
+function EditProfileForm() {
 
-                        <Grid.Row>
-                            <Grid.Column width={4} className="edit-profile-col">
-                                EMAIL
-                            </Grid.Column>
-                            <Grid.Column>
-                                <input className="edit-profile-input" type="text" />
-                            </Grid.Column>
-                        </Grid.Row>
+  return (
+      <div>
+        <Container>
+            <Grid stackable columns={2} style={{ paddingLeft: '20px', paddingRight: '20px', paddingBottom: '20px' }}>
+                <Grid.Row>
+                    <Grid.Column width={4} className="edit-profile-col">
+                        NAME
+                    </Grid.Column>
+                    <Grid.Column>
+                        <input className="edit-profile-input" type="text" />
+                    </Grid.Column>
+                </Grid.Row>
 
-                        <Grid.Row>
-                            <Grid.Column width={4} className="edit-profile-col">
-                                USERNAME
-                            </Grid.Column>
-                            <Grid.Column>
-                                <input className="edit-profile-input" type="text" />
-                            </Grid.Column>
-                        </Grid.Row>
+                <Grid.Row>
+                    <Grid.Column width={4} className="edit-profile-col">
+                        EMAIL
+                    </Grid.Column>
+                    <Grid.Column>
+                        <input className="edit-profile-input" type="text" />
+                    </Grid.Column>
+                </Grid.Row>
 
-                        <Grid.Row>
-                          <Grid.Column width={4} className="edit-profile-col">
-                            PASSWORD
-                          </Grid.Column>
-                          <Grid.Column >
-                            <input className="edit-profile-input" type="password" placeholder="ENTER CURRENT PASSWORD"/>
-                          </Grid.Column>
-                        </Grid.Row>
+                <Grid.Row>
+                    <Grid.Column width={4} className="edit-profile-col">
+                        USERNAME
+                    </Grid.Column>
+                    <Grid.Column>
+                        <input className="edit-profile-input" type="text"/>
+                    </Grid.Column>
+                </Grid.Row>
 
-                      <Grid.Row>
-                        <Grid.Column width={4}>
-                        </Grid.Column>
-                        <Grid.Column>
-                          <input className="edit-profile-input" type="password" placeholder="ENTER NEW PASSWORD"/>
-                        </Grid.Column>
-                      </Grid.Row>
+                <Grid.Row>
+                  <Grid.Column width={4} className="edit-profile-col">
+                    PASSWORD
+                  </Grid.Column>
+                  <Grid.Column >
+                    <input className="edit-profile-input" type="password" placeholder="ENTER CURRENT PASSWORD"/>
+                  </Grid.Column>
+                </Grid.Row>
 
-                      <Grid.Row>
-                        <Grid.Column width={4}>
-                        </Grid.Column>
-                        <Grid.Column>
-                          <input className="edit-profile-input" type="password" placeholder="RETYPE NEW PASSWORD"/>
-                        </Grid.Column>
-                      </Grid.Row>
+              <Grid.Row>
+                <Grid.Column width={4}>
+                </Grid.Column>
+                <Grid.Column>
+                  <input className="edit-profile-input" type="password" placeholder="ENTER NEW PASSWORD"/>
+                </Grid.Column>
+              </Grid.Row>
 
-                    </Grid>
+              <Grid.Row>
+                <Grid.Column width={4}>
+                </Grid.Column>
+                <Grid.Column>
+                  <input className="edit-profile-input" type="password" placeholder="RETYPE NEW PASSWORD"/>
+                </Grid.Column>
+              </Grid.Row>
 
-                    <Button inverted color="white">SAVE</Button>
-                </Container>
-            </div>
-        );
-    }
+            </Grid>
+
+            <Button inverted>SAVE</Button>
+        </Container>
+    </div>
+  );
 }
 
 export default EditProfileForm;
--- app/imports/ui/pages/EditProfile.jsx
+++ app/imports/ui/pages/EditProfile.jsx
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import EditProfileForm from '../components/EditProfileForm';
+import NavBarHome from '../components/NavBarHome';
 
-class EditProfile extends Component {
-    render() {
-        return (
-            <div style={{ textAlign: 'center', background: '#20639B', minHeight: '80vh', marginTop: '-10px' }}>
-                <h1 style={{ color: 'white', paddingTop: '20vh', paddingBottom: '2rem', fontSize: '36px' }}><u>EDIT PROFILE</u></h1>
-                <EditProfileForm/>
-            </div>
-        );
-    }
+function EditProfile() {
+    return (
+        <div style={{ textAlign: 'center', background: '#20639B', minHeight: '80vh', marginTop: '-10px' }}>
+            <NavBarHome/>
+            <h1 style={{ color: 'white', paddingTop: '20vh', paddingBottom: '2rem', fontSize: '36px' }}><u>EDIT PROFILE</u></h1>
+            <EditProfileForm/>
+        </div>
+    );
 }
 
 export default EditProfile;
