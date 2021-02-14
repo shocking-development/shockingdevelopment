60c7fac5bd3dfbf179cf2e131da825b0110aa774
Shun <47809066+saitoshi@users.noreply.github.com>
2021-02-14 09:34:07 -1000

    Update Publications.js

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 import { Stuffs } from '../../api/stuff/Stuff';
+import { UserInfo } from '../../api/userInfo/userInfo';
 
 // User-level publication.
 // If logged in, then publish documents owned by this user. Otherwise publish nothing.
@@ -17,6 +18,23 @@ Meteor.publish(Stuffs.userPublicationName, function () {
 Meteor.publish(Stuffs.adminPublicationName, function () {
   if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
     return Stuffs.collection.find();
+  }
+  return this.ready();
+});
+
+// Publish the User info
+Meteor.publish('UserInfo', function publish(){
+  if (this.userId) {
+    const username = Meteor.users.findOne(this.userId).username;
+    return UserInfo.find({owner: username});
+  }
+  return this.ready();
+});
+
+// Admin Level publication for the user info
+Meteor.publish(UserInfo.adminPublicationName, function() {
+  if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
+    return UserInfo.collection.find();
   }
   return this.ready();
 });
