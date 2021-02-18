e2de8ba0198b53d3e40af3d5c8a1f897cd175d80
trey-sumida <tasumida@hawaii.edu>
2021-02-17 20:25:00 -1000

    Fixed padding issue

--- app/imports/ui/pages/AddData.jsx
+++ app/imports/ui/pages/AddData.jsx
@@ -8,7 +8,7 @@ function AddData() {
   return (
       <div style={{ marginTop: '-10px' }}>
         <NavBarHome/>
-        <div style={{ textAlign: 'center', background: '#174060', minHeight: '80vh', minWidth: '100%' }}>
+        <div style={{ textAlign: 'center', background: '#174060', minHeight: '80vh', Width: '100%', paddingLeft: '15em' }}>
           <h1 style={{ color: 'white', paddingTop: '10vh', paddingBottom: '2rem', fontSize: '36px' }}><u>ADD DATA</u>
           </h1>
           <StaticData/>
