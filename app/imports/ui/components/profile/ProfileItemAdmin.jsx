fd17ddb1b626d385b7825827aebe87eff28ccbfd
CalianaFortin <70739998+CalianaFortin@users.noreply.github.com>
2021-02-20 21:15:30 -1000

    admin list works
    - need remove button to remove the user

---
+++ app/imports/ui/components/profile/ProfileItemAdmin.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Button, Table } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { Meteor } from 'meteor/meteor';
+import { NavLink } from 'react-router-dom';
+
+/** Renders a single row in the List Stuff (Admin) table. See pages/ListProfileAdmin.jsx. */
+class ProfileItemAdmin extends React.Component {
+
+  removeUser(docID) {
+    Meteor.users.remove(docID);
+  }
+
+  render() {
+    return (
+        <Table.Row>
+          <Table.Cell>{this.props.profile.userName}</Table.Cell>
+          <Table.Cell>{this.props.profile.firstName}</Table.Cell>
+          <Table.Cell>{this.props.profile.lastName}</Table.Cell>
+          <Table.Cell>{this.props.profile.email}</Table.Cell>
+          <Table.Cell>{this.props.profile.zipcode}</Table.Cell>
+          <Table.Cell>{this.props.profile.owner}</Table.Cell>
+          <Table.Cell>
+            <Button as={NavLink} exact to='/admin' floated='right'
+                    data={this.props.profile._id}
+                    onClick={(data) => this.removeUser(data)} color='red'
+            >
+              Delete
+            </Button>
+          </Table.Cell>
+        </Table.Row>
+    );
+  }
+}
+
+/** Require a document to be passed to this component. */
+ProfileItemAdmin.propTypes = {
+  profile: PropTypes.object.isRequired,
+};
+
+export default ProfileItemAdmin;
