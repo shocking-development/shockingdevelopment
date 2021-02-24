394bd3bc35df0f3b27a650e92d9d29ef585cd341
trey-sumida <tasumida@hawaii.edu>
2021-02-23 16:26:08 -1000

    Fixed mpg text size

--- app/imports/ui/components/StaticData.jsx
+++ app/imports/ui/components/StaticData.jsx
@@ -3,7 +3,7 @@ import { Button } from 'semantic-ui-react';
 
 function StaticData() {
 
-    const dataInput = { width: '4rem', height: '3rem', borderRadius: '5px', borderWidth: '0', background: '#C4C4C4' };
+    const dataInput = { width: '4rem', height: '3rem', borderRadius: '5px', borderWidth: '0', background: '#C4C4C4', fontSize: '2rem', textAlign: 'center' };
     const centerText = { textAlign: 'center', color: 'white' };
 
     return (
