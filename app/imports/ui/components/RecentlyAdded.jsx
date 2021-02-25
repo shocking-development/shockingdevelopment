dbb0e85c82480bacaee79884951c62c8904bc350
trey-sumida <tasumida@hawaii.edu>
2021-02-24 23:18:53 -1000

    Fixed most of the css
    .column class in style.css is messing up the styling

--- app/imports/ui/components/RecentlyAdded.jsx
+++ app/imports/ui/components/RecentlyAdded.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
-import { Card, Header } from 'semantic-ui-react';
+import { Card, Header, Grid } from 'semantic-ui-react';
 import { DataCollection } from '../../api/data/DataCollection';
 
 function RecentlyAdded() {
@@ -13,18 +13,26 @@ function RecentlyAdded() {
     });
 
     return (
-        <div style={{ paddingTop: '3rem', width: '100%', justifyContent: 'center' }}>
+        <div style={{ paddingTop: '3rem' }}>
             <Header inverted size='huge' textAlign={'center'}>Recently Added</Header>
-            {data.map(recentData => <Card key={toString(recentData.date)} style={{ padding: '1rem', background: '#4282AF' }}>
-                    <Card.Content>
-                    <Card.Header style={{ color: 'white' }}>{recentData.date}</Card.Header>
-                    </Card.Content>
-                    <Card.Content style={{ color: 'white' }}>
-                    <b>Transportation: {recentData.transportation}</b>
-                    <br/>
-                    <b>Miles: {recentData.miles}</b>
-                    </Card.Content>
-                    </Card>)}
+
+            <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+                <Grid>
+                    <Grid.Row columns={3}>
+                        {data.map(recentData => <Grid.Column key={toString(recentData.createdAt) + recentData.transportation + recentData.miles}>
+                        <Card style={{ padding: '1rem', background: '#4282AF' }}>
+                        <Card.Content>
+                        <Card.Header style={{ color: 'white' }}>{recentData.date}</Card.Header>
+                        </Card.Content>
+                        <Card.Content style={{ color: 'white' }}>
+                        <b>Transportation: {recentData.transportation}</b>
+                        <br/>
+                        <b>Miles: {recentData.miles}</b>
+                        </Card.Content>
+                        </Card></Grid.Column>)}
+                    </Grid.Row>
+                </Grid>
+            </div>
         </div>
     );
 }
