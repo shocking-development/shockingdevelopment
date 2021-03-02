5261c41446254cd91c9f5bbd0f5ddf5502cd6b71
CalianaFortin <70739998+CalianaFortin@users.noreply.github.com>
2021-03-02 13:42:01 -1000

    Update NavBarLanding.jsx

--- app/imports/ui/components/landing/NavBarLanding.jsx
+++ app/imports/ui/components/landing/NavBarLanding.jsx
@@ -1,82 +1,95 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
-import { withTracker } from 'meteor/react-meteor-data';
-import { withRouter, NavLink } from 'react-router-dom';
+import { useTracker } from 'meteor/react-meteor-data';
+import { NavLink } from 'react-router-dom';
 import { Menu, Dropdown, Image } from 'semantic-ui-react';
 
 /**
  * The NavBar appears at the top of the Landing page. Rendered by the Landing page.
  * @memberOf ui/components/landing
  */
-class NavBarLanding extends React.Component { // implement function and have boolean for user/admin
-  render() {
-    /* Some styling components */
-    const menuStyle = { height: '10px', paddingTop: '10px' };
-    const loginButton = {
-      color: '#3CAEA3',
-      fontSize: '1.2375em',
-      letterSpacing: '2px',
-      fontWeight: 'bold',
-      zIndex: 'auto',
-      position: 'absolute',
-      top: '1.7em',
-      left: '85%',
-      paddingRight: '1em',
+function NavBarLanding() { // implement function and have boolean for user/admin
 
+  /* Some styling components */
+  const menuStyle = { height: '10px', paddingTop: '10px' };
+  const loginButton = {
+    color: '#3CAEA3',
+    fontSize: '1.2375em',
+    letterSpacing: '2px',
+    fontWeight: 'bold',
+    zIndex: 'auto',
+    position: 'absolute',
+    top: '1.7em',
+    left: '85%',
+    paddingRight: '1em',
+
+  };
+  const logo = {
+    width: '100%',
+    marginTop: '25px',
+    paddingLeft: '45px',
+    height: 'auto',
+    maxWidth: '155px',
+    left: '6%',
+  };
+
+  /* Gets the current user and tells us if we are logging in */
+  const { user, isLoggedIn } = useTracker(() => {
+    // eslint-disable-next-line no-shadow
+    const user = Meteor.user();
+    const userId = Meteor.userId(); // gets the id of the user
+    const isLoggingIn = Meteor.loggingIn();
+    return {
+      user,
+      userId,
+      isLoggingIn,
+      isLoggedIn: !!userId, // if the user logged in does not equal the userId then we are signing in
     };
-    const logo = {
-      width: '100%',
-      marginTop: '25px',
-      paddingLeft: '45px',
-      height: 'auto',
-      maxWidth: '155px',
-      left: '6%',
-    };
+  });
 
-    return (
-        <div id='navbar-landing' style={menuStyle}>
+  if (isLoggedIn) { // if we are signing in then render this else
+    return <div id='navbar-landing' style={menuStyle}>
 
-          <Menu.Item as={NavLink} activeClassName="" exact to="/">
-            <Image src='/images/HEI-LOGO.png' style={logo}/>
-          </Menu.Item>
+      <Menu.Item as={NavLink} activeClassName="" exact to="/">
+        <Image src='/images/HEI-LOGO.png' style={logo}/>
+      </Menu.Item>
 
-          <Menu.Item>
-            {this.props.currentUser === '' ? (
-                <Dropdown className='body' id="login-dropdown" text="LOGIN" pointing="top right" style={loginButton}>
-                  <Dropdown.Menu>
-                    <Dropdown.Item id="login-dropdown-sign-in" icon="user" text="Sign In" as={NavLink} exact
-                                   to="/signin"/>
-                    <Dropdown.Item id="login-dropdown-sign-up" icon="add user" text="Sign Up" as={NavLink} exact
-                                   to="/signup"/>
-                  </Dropdown.Menu>
-                </Dropdown>
-            ) : (
-                <Dropdown className='body' id="navbar-current-user" text={this.props.currentUser} pointing="top right"
-                          icon={'user'}
-                          style={loginButton}>
-                  <Dropdown.Menu>
-                    <Dropdown.Item id="navbar-sign-out" icon="sign out" text="Sign Out" as={NavLink} exact
-                                   to="/signout"/>
-                  </Dropdown.Menu>
-                </Dropdown>
-            )}
-          </Menu.Item>
+      <Menu.Item>
 
-        </div>
-    );
+        <Dropdown className='body' id="navbar-current-user" text={user} pointing="top right"
+                  icon={'user'}
+                  style={loginButton}>
+          <Dropdown.Menu>
+            <Dropdown.Item id="navbar-sign-out" icon="sign out" text="Sign Out" as={NavLink} exact
+                           to="/signout"/>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Menu.Item>
+
+    </div>;
   }
+
+  return (
+      <div id='navbar-landing' style={menuStyle}>
+
+        <Menu.Item as={NavLink} activeClassName="" exact to="/">
+          <Image src='/images/HEI-LOGO.png' style={logo}/>
+        </Menu.Item>
+
+        <Menu.Item>
+
+          <Dropdown className='body' id="login-dropdown" text="LOGIN" pointing="top right" style={loginButton}>
+            <Dropdown.Menu>
+              <Dropdown.Item id="login-dropdown-sign-in" icon="user" text="Sign In" as={NavLink} exact
+                             to="/signin"/>
+              <Dropdown.Item id="login-dropdown-sign-up" icon="add user" text="Sign Up" as={NavLink} exact
+                             to="/signup"/>
+            </Dropdown.Menu>
+          </Dropdown>
+
+        </Menu.Item>
+      </div>
+  );
 }
 
-/** Declare the types of all properties. */
-NavBarLanding.propTypes = {
-  currentUser: PropTypes.string,
-};
-
-/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
-const NavBarLandingContainer = withTracker(() => ({
-  currentUser: Meteor.user() ? Meteor.user().username : '',
-}))(NavBarLanding);
-
-/** Enable ReactRouter for this component. https://reacttraining.com/react-router/web/api/withRouter */
-export default withRouter(NavBarLandingContainer);
+export default NavBarLanding;
