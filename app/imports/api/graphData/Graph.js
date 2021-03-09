2a8b064c51fe4e7fc5cde99b6db4f5b38f0ebec9
Jamse Galway-Sevetson <jtmoka@hawaii.edu>
2021-03-08 19:31:57 -1000

    Update Graph.js

--- app/imports/api/graphData/Graph.js
+++ app/imports/api/graphData/Graph.js
@@ -1,14 +1,17 @@
+import React from 'react';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 import { Mongo } from 'meteor/mongo';
 import SimpleSchema from 'simpl-schema';
 import { Tracker } from 'meteor/tracker';
 
 /** Encapsulates state and variable values for this collection. */
 class GraphsCollection {
-  constructor() {
 
+  constructor() {
     // The name of this collection.
     this.name = 'GraphsCollection';
-    this.dataOne = [20, 71.5, 0, 0, 0, 0, 80, 0, 0, 0, 0, 0];
+    this.dataOne = [20, 71.5, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0];
     this.dataTwo = [83.6, 78.8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     this.dataThree = [48.9, 38.8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
@@ -22,6 +25,7 @@ class GraphsCollection {
       dataTwo: Number,
       dataThree: Number,
       owner: String,
+      fontFamily: 'sans-serif',
     }, { tracker: Tracker });
 
     // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
