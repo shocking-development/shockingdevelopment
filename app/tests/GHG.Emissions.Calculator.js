93576001edfab1b1a89511f8e4c9f9e89b8b84f7
mkirts <70680675+mkirts@users.noreply.github.com>
2021-03-15 15:59:06 -1000

    Minor Changes to GHG.Emissions.Calculator
    - Changing tags
    - Change class names
    - Skeleton test cases

--- app/tests/GHG.Emissions.Calculator.js
+++ app/tests/GHG.Emissions.Calculator.js
@@ -1,8 +1,8 @@
 import { Selector } from 'testcafe';
 
-class LandingPage {
+class GHGEmissionsCalculator {
   constructor() {
-    this.pageId = '#landing-page';
+    this.pageId = '#calculator';
     this.pageSelector = Selector(this.pageId);
   }
 
@@ -11,6 +11,12 @@ class LandingPage {
     // This is first test to be run. Wait 10 seconds to avoid timeouts with GitHub Actions.
     await testController.wait(10000).expect(this.pageSelector.exists).ok();
   }
+
+  async hasTable(testController) {
+    const rowCount = Selector('tr').count;
+    await testController.wait(10000).expect(this.pageSelector.exists).ok();
+    await testController.expect(rowCount).get(2);
+  }
 }
 
-export const landingPage = new LandingPage();
+export const emissionsCalculator = new GHGEmissionsCalculator();
