cc81a1f146c7af2920efc05bbf2b1099fa21d62b
Jamse Galway-Sevetson <jtmoka@hawaii.edu>
2021-03-16 17:04:04 -1000

    Update GraphsCollection.js

--- app/imports/api/graphs/GraphsCollection.js
+++ app/imports/api/graphs/GraphsCollection.js
@@ -1,11 +1,14 @@
 import { Mongo } from 'meteor/mongo';
 import SimpleSchema from 'simpl-schema';
 import { Tracker } from 'meteor/tracker';
-import { Stuffs } from '../stuff/Stuff';
-import StuffItem from '../../ui/components/StuffItem';
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+import { _ } from 'meteor/underscore';
+import { Roles } from 'meteor/alanning:roles';
+import { carsPublications } from '../cars/CarsCollection';
 
-/** Encapsulates state and variable values for this collection. */
-class GraphsCollection {
+// Encapsulates state and variable values for this collection.
+class GraphsCollection{
 
   constructor() {
 
@@ -13,12 +16,9 @@ class GraphsCollection {
     this.name = 'BarGraphCollection';
 
     //Current plays holder data
-    this.dataOne = [
-        [20, 71.5, 0, 0, 55.8, 0, 8, 0, 0, 0, 0, 30],
-        [83.6, 78.8, 0, 0, 0, 20, 0, 0, 0, 0, 0, 30],
-        [48.9, 38.8, 0, 0, 0, 0, 0, 0, 90, 0, 0, 30]
-    ];
-
+    this.dataOne = [20, 71.5, 0, 0, 55.8, 0, 8, 0, 0, 0, 0, 30];
+    this.dataTwo = [83.6, 78.8, 0, 0, 0, 20, 0, 0, 0, 0, 0, 30];
+    this.dataThree = [48.9, 38.8, 0, 0, 0, 0, 0, 0, 90, 0, 0, 30];
 
     // Define the Mongo collection.
     this.collection = new Mongo.Collection(this.name);
@@ -26,9 +26,10 @@ class GraphsCollection {
     // Define the structure of each document in the collection.
     this.schema = new SimpleSchema({
       name: String,
-      dataOnes: Array[Array[Number], Array[Number], Array[Number]],
+      dataOne: Array[Number],
+      dataTwo: Array[Number],
+      dataThree: Array[Number],
       owner: String,
-      fontFamily: 'sans-serif',
     }, { tracker: Tracker });
 
     // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
@@ -38,6 +39,68 @@ class GraphsCollection {
     this.userPublicationName = `${this.name}.publication.user`;
     this.adminPublicationName = `${this.name}.publication.admin`;
   }
+
+  define({ name, dataOne, owner }) {
+    const instance = this._collection.insert({
+      name,
+      dataOne,
+      owner,
+    });
+    return instance;
+  }
+
+  update(instance, { name, dataOne, owner }) {
+    const dataUpdate = {};
+
+    // update the name of the graph
+      dataUpdate.name = name;
+
+    // update the owner of the data
+      dataUpdate.dataOne = dataOne;
+
+    // update the owner of the graph
+      dataUpdate.owner = owner;
+
+    this._collection.update(instance, { $set: updateData });
+  }
+
+  removeIt(instance) {
+    const doc = this.findDoc(instance);
+    check(doc, Object);
+    this.collection.remove(doc._id);
+    return true;
+  }
+
+  publish() {
+    if (Meteor.isServer) {
+      // get the StuffCollection instance.
+      const instance = this;
+      /** This subscription publishes only the documents associated with the logged in user */
+      Meteor.publish(carsPublications.car, function publish() {
+        if (this.userId) {
+          return instance._collection.find({});
+        }
+        return this.ready();
+      });
+
+      /** This subscription publishes all documents regardless of user, but only if the logged in user is the Admin. */
+      Meteor.publish(carsPublications.carsAdmin, function publish() {
+        if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
+          return instance._collection.find();
+        }
+        return this.ready();
+      });
+    }
+  }
+
+  subscribeCars() {
+    if (Meteor.isClient) {
+      return Meteor.subscribe(carsPublications.car);
+    }
+    return null;
+  }
+
+
 }
 
 //Export Collection
