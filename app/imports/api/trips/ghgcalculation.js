2f615c47bf2f25d9ce568342fae66e8dd62fd155
CalianaFortin <70739998+CalianaFortin@users.noreply.github.com>
2021-03-23 00:55:18 -1000

    imported necessary stuff

--- app/imports/api/trips/ghgcalculation.js
+++ app/imports/api/trips/ghgcalculation.js
@@ -7,11 +7,6 @@ export function calculateCO2(gallons) {
   return (gallons * (8.887 * mutltifactor)).toFixed(2);
 }
 
-/** This function calculates the CO2 created by user inputted gallons. */
-export function calculateGallonsUsed(miles, mpg) {
-  return (miles / mpg).toFixed(2);
-}
-
 /** This function calculates the GHG emissions equivalency. */
 export function calculateGHG(gallons) {
   const mutltifactor = 0.00043;
@@ -21,7 +16,13 @@ export function calculateGHG(gallons) {
 /** This function converts liters into gallons. */
 export function calculateGal(liters) {
   const convertFactor = 0.264172;
-  return (liters * convertFactor);
+  return (liters * convertFactor).toFixed(4);
+}
+
+/** This function convers tons into metric tons. */
+export function convertTons(usTon) {
+  const convertFactor = 0.907185;
+  return (usTon * convertFactor).toFixed(2);
 }
 
 // distance and mpg calculates number of gallons
