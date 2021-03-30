3d34d60923f619fa2d58f4383d3edbbaa34332a9
mkirts <70680675+mkirts@users.noreply.github.com>
2021-03-29 22:29:54 -1000

    Changes to Testcafe
    Minor changes to testcafe: replace await testController... with this.isDisplayed

--- app/tests/Profile.page.js
+++ app/tests/Profile.page.js
@@ -2,7 +2,7 @@ import { Selector } from 'testcafe';
 
 class ProfilePage {
   constructor() {
-    this.pageId = '#landing-page';
+    this.pageId = '#user-profile';
     this.pageSelector = Selector(this.pageId);
   }
 
@@ -11,17 +11,20 @@ class ProfilePage {
     // This is first test to be run. Wait 10 seconds to avoid timeouts with GitHub Actions.
     await testController.wait(10000).expect(this.pageSelector.exists).ok();
   }
+
   // Click to check if user profile exists
   async visitProfile(testController) {
-    await testController.wait(10000).expect(this.pageSelector.exists).ok();
+    await this.isDisplayed(testController);
   }
+
   // Check for edit portion of user profile
   async editProfile(testController) {
-    await testController.wait(10000).expect(this.pageSelector.exists).ok();
+    await this.isDisplayed(testController);
   }
+
   // Check if can edit/change users password
-  async profilePassword(testController){
-    await testController.wait(10000).expect(this.pageSelector.exists).ok();
+  async profilePassword(testController) {
+    await this.isDisplayed(testController);
   }
 }
 
