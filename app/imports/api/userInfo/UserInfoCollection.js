d3532230f6c148337b8ed05e6380a9e2261d4797
CalianaFortin <70739998+CalianaFortin@users.noreply.github.com>
2021-04-02 07:08:25 -1000

    added some necessary stuff for calculations

--- app/imports/api/userInfo/UserInfoCollection.js
+++ app/imports/api/userInfo/UserInfoCollection.js
@@ -28,6 +28,17 @@ class UserInfoCollection extends BaseCollection {
         allowedValues: ['metric', 'us units'],
         defaultValue: 'us units',
       },
+      state: {
+        type: String,
+        allowedValues: ['Alaska', 'Alabama', 'Arkansas', 'Arizona', 'California', 'Colorado', 'Connecticut',
+          'District of Columbia', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Iowa', 'Idaho', 'Illinois',
+          'Indiana', 'Kansas', 'Kentucky', 'Louisiana', 'Massachusetts', 'Maryland', 'Maine', 'Michigan',
+          'Minnesota', 'Missouri', 'Mississippi', 'Montana', 'North Carolina', 'North Dakota', 'Nebraska',
+          'New Hampshire', 'New Jersey', 'New Mexico', 'Nevada', 'New York', 'Ohio', 'Oklahoma', 'Oregon',
+          'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah',
+          'Virginia', 'Vermont', 'Washington', 'Wisconsin', 'West Virginia', 'Wyoming'],
+        defaultValue: 'Hawaii',
+      },
     }));
   }
 
