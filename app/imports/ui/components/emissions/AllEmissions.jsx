5b5b4900bcedc0638efd0a1434b0f14e59c25dbb
trey-sumida <tasumida@hawaii.edu>
2021-04-04 05:20:46 -1000

    pagination works

--- app/imports/ui/components/emissions/AllEmissions.jsx
+++ app/imports/ui/components/emissions/AllEmissions.jsx
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
-import { Table, Button } from 'semantic-ui-react';
+import { Table, Pagination } from 'semantic-ui-react';
 import { Emissions } from '../../../api/emissions/EmissionsCollection';
-import { EmissionsRemoveMethod } from '../../../api/emissions/EmissionsCollection.methods';
+import EmissionItem from './EmissionItem';
 
 function AllEmissions() {
 
-    const user = useTracker(() => Meteor.userId());
+    const [activePage, setActivePage] = useState(1);
+    const startIndex = (activePage * 10 - 10);
+    const endIndex = (activePage * 10);
+
     const emissions = useTracker(() => {
-      Meteor.subscribe(Emissions.emissionsPublicationName);
-      return Emissions.collection.find({ owner: user }, { sort: { createdAt: -1 } }).fetch();
+        Meteor.subscribe(Emissions.emissionsPublicationName);
+        const user = Meteor.userId();
+        return Emissions.collection.find({ owner: user }, { sort: { createdAt: -1 } }).fetch();
     });
 
-    const deleteEmission = ({ _id }) => EmissionsRemoveMethod.call(_id);
+    const handleInputChange = (e, data) => {
+        setActivePage(Number(data.activePage));
+    };
 
     return (
         <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
         <div style={{ width: '73%' }}>
+            <Pagination
+                defaultActivePage={1}
+                totalPages={Math.ceil(emissions.length / 10)}
+                onPageChange={handleInputChange}
+            />
               <Table>
                 <Table.Header>
                 <Table.Row>
@@ -29,12 +40,7 @@ function AllEmissions() {
                 </Table.Header>
 
                 <Table.Body>
-                    {emissions.map(emission => <Table.Row key={emission._id}>
-                        <Table.Cell>{`${emission.date.getMonth() + 1}/${emission.date.getDate()}/${emission.date.getFullYear()}`}</Table.Cell>
-                        <Table.Cell>{emission.transportation}</Table.Cell>
-                        <Table.Cell>{emission.miles}</Table.Cell>
-                        <Table.Cell><Button inverted color='red' onClick={() => deleteEmission(emission)}>Delete</Button></Table.Cell>
-                    </Table.Row>)}
+                    {emissions.map(emission => <EmissionItem key={emission._id} emission={emission}/>).slice(startIndex, endIndex)}
                 </Table.Body>
             </Table>
         </div>
---
+++ app/imports/ui/components/emissions/EmissionItem.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Table, Button } from 'semantic-ui-react';
+import { EmissionsRemoveMethod } from '../../../api/emissions/EmissionsCollection.methods';
+
+function EmissionItem({ emission }) {
+
+    const deleteEmission = ({ _id }) => EmissionsRemoveMethod.call(_id);
+
+    return (
+        <Table.Row>
+            <Table.Cell>{`${emission.date.getMonth() + 1}/${emission.date.getDate()}/${emission.date.getFullYear()}`}</Table.Cell>
+            <Table.Cell>{emission.transportation}</Table.Cell>
+            <Table.Cell>{emission.miles}</Table.Cell>
+            <Table.Cell><Button inverted color='red' onClick={() => deleteEmission(emission)}>Delete</Button></Table.Cell>
+        </Table.Row>
+    );
+}
+
+EmissionItem.propTypes = {
+    emission: PropTypes.object,
+};
+
+export default EmissionItem;
