bedeaaa7d1f6f247d4dbbf77e06ad804a8ef7619
Shun <47809066+saitoshi@users.noreply.github.com>
2021-04-11 18:01:47 -1000

    Create FakeData.js

---
+++ app/imports/api/userInfo/FakeData.js
@@ -0,0 +1,8 @@
+import { Meteor } from 'meteor/meteor';
+import SimpleSchema from 'simpl-schema';
+import { check } from 'meteor/check';
+import { _ } from 'meteor/underscore';
+import { Roles } from 'meteor/alanning:roles';
+import BaseCollection from '../base/BaseCollection';
+
+
