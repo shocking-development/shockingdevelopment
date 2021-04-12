38a661599d7b0c651bb548da85696c64a459aacb
trey-sumida <tasumida@hawaii.edu>
2021-04-12 12:47:42 -1000

    User must confirm before deleting emission

--- app/imports/ui/components/emissions/EmissionItem.jsx
+++ app/imports/ui/components/emissions/EmissionItem.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import swal from 'sweetalert';
 import { Table, Button } from 'semantic-ui-react';
 import { EmissionsRemoveMethod } from '../../../api/emissions/EmissionsCollection.methods';
 
 function EmissionItem({ emission }) {
 
-    const deleteEmission = ({ _id }) => EmissionsRemoveMethod.call(_id);
+    const deleteEmission = ({ _id }) => {
+        swal({
+            text: `Are you sure you want to delete ${emission.transportation} for ${emission.miles}mi on ${emission.date.getMonth() + 1}/${emission.date.getUTCDate()}/${emission.date.getFullYear()}?`,
+            buttons: {
+                cancel: {
+                    text: 'Cancel',
+                    value: false,
+                    visible: true,
+                },
+                confirm: {
+                    text: 'Confirm',
+                    value: true,
+                    visible: true,
+                },
+            },
+        }).then((val) => {
+            if (val) {
+                EmissionsRemoveMethod.call(_id);
+            }
+          });
+    };
 
     return (
         <Table.Row>
