f61d22b31c871cc7d5171dff5ac74951f6e5a49e
mkirts <70680675+mkirts@users.noreply.github.com>
2021-04-18 19:40:33 -1000

    Added "Requests Page". No updates to the navbar yet, however.

---
+++ app/imports/ui/pages/cars/Requests.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Header, Grid, Segment } from 'semantic-ui-react';
+import SimpleSchema from 'simpl-schema';
+import { SimpleSchema2Bridge } from 'uniforms-bridge-simple-schema-2';
+import { AutoForm, SubmitField, TextField, ErrorsField } from 'uniforms-semantic';
+import { Meteor } from 'meteor/meteor';
+import swal from 'sweetalert';
+
+const formSchema = new SimpleSchema({
+  recommendationType: String,
+  recommendation: String,
+});
+
+const bridge = new SimpleSchema2Bridge(formSchema);
+
+class Requests extends React.Component {
+  submit(data, formRef) {
+    const { recommendationType, recommendation } = data;
+    const owner = Meteor.user().username;
+    const username = owner;
+    User.collection.insert({ username, recommendationType, recommendation},
+        (error) => {
+          if (error) {
+            swal('Error', error.message, 'error');
+          } else {
+            swal('Success', 'Recommendation processed', 'success');
+            formRef.reset();
+          }
+        });
+  }
+
+  render() {
+    let fRef = null;
+    return (
+        <Grid container centered>
+          <Grid.Column>
+            <Header as="h2" textAlign="center">Create Profile</Header>
+            <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => this.submit(data, fRef)} >
+              <Segment>
+                <TextField name='recommendationType' label='Type of Recommendation'/>
+                <TextField name='recommendation' label='Type your recommendation here'/>
+                <SubmitField value='Submit'/>
+                <ErrorsField/>
+              </Segment>
+            </AutoForm>
+          </Grid.Column>
+        </Grid>
+    );
+  }
+}
+
+export default Requests;
