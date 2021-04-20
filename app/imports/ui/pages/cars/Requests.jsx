a5fea8bcb2b2258ab1b434bdd0c601c3ce5ea9bd
mkirts <70680675+mkirts@users.noreply.github.com>
2021-04-19 14:39:59 -1000

    Added CSS updates

--- app/imports/ui/pages/cars/Requests.jsx
+++ app/imports/ui/pages/cars/Requests.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Header, Grid, Segment } from 'semantic-ui-react';
+import { Header, Grid, Segment, Image, Button, Form } from 'semantic-ui-react';
 import SimpleSchema from 'simpl-schema';
 import { SimpleSchema2Bridge } from 'uniforms-bridge-simple-schema-2';
 import { AutoForm, SubmitField, TextField, ErrorsField } from 'uniforms-semantic';
 import { Meteor } from 'meteor/meteor';
 import swal from 'sweetalert';
 import { newRequest } from '../../../api/cars/UserRequest';
+import NavBarMain from '../../components/main-navbar/NavBarMain';
 
 const formSchema = new SimpleSchema({
   recommendationType: String,
@@ -13,6 +14,18 @@ const formSchema = new SimpleSchema({
 });
 
 const bridge = new SimpleSchema2Bridge(formSchema);
+const pageStyle = {
+  background: '#001947',
+  height: '100vh',
+  backgroundSize: 'cover',
+  paddingTop: '5em',
+  paddingLeft: '10%',
+  justifyContent: 'center',
+};
+
+const buttonStyle = {
+  justifyContent: 'center',
+};
 
 class Requests extends React.Component {
   submit(data, formRef) {
@@ -33,19 +46,25 @@ class Requests extends React.Component {
   render() {
     let fRef = null;
     return (
+        <div style={pageStyle}>
+          <NavBarMain/>
         <Grid container centered>
           <Grid.Column>
-            <Header as="h2" textAlign="center">Create Profile</Header>
+            <Header as="h2" textAlign="center" inverted>Make a Request</Header>
+            <Image src='images/HEI-WAVE-LOGO.png' centered size='small' style={{
+              paddingBottom: '50px',
+            }}/>
             <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => this.submit(data, fRef)} >
               <Segment>
                 <TextField name='recommendationType' label='Type of Recommendation'/>
                 <TextField name='recommendation' label='Type your recommendation here'/>
-                <SubmitField value='Submit'/>
+                <Button color='blue' style={buttonStyle} onClick={this.showResult} id='submit-request'>Submit Request</Button>
                 <ErrorsField/>
               </Segment>
             </AutoForm>
           </Grid.Column>
         </Grid>
+        </div>
     );
   }
 }
