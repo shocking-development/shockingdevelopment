503d6236a22fa41b64a496556ab4439b98cf6aec
kyraikeda <kyraikeda@gmail.com>
2021-04-21 22:06:03 -1000

    fixed error alerts and css

--- app/imports/ui/pages/profile/ChangePassword.jsx
+++ app/imports/ui/pages/profile/ChangePassword.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Loader, Header, Image, Segment, Container, Card, Form } from 'semantic-ui-react';
+import { Loader, Header, Image, Segment, Container, Grid, Form } from 'semantic-ui-react';
 import swal from 'sweetalert';
 // eslint-disable-next-line no-unused-vars
 import { ErrorsField, HiddenField, NumField, SelectField, SubmitField, TextField } from 'uniforms-semantic';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
-import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 import { UserInfos } from '../../../api/userInfo/UserInfoCollection';
 import NavBarHome from '../../components/main-navbar/NavBarMain';
@@ -13,42 +12,40 @@ import NavBarHome from '../../components/main-navbar/NavBarMain';
 /** Renders the Page for editing a single document. */
 class ChangePassword extends React.Component {
 
-  /** Initialize state fields. */
+  /* Initialize state fields. */
   constructor(props) {
     super(props);
-    this.state = { currPass: '', newPass: '', confirm: '', error: '', redirectToReferer: false };
+    this.state = { currPass: '', newPass: '', confirm: '', redirectToReferer: false };
   }
 
-  /** Update the form controls each time the user interacts with them. */
+  /* Update the form controls each time the user interacts with them. */
   handleChange = (e, { name, value }) => {
     this.setState({ [name]: value });
   };
 
-  /** On successful submit, insert the data. */
+  /* On successful submit, insert the data. */
   submit = () => {
-    const { currPass, newPass, confirm, error } = this.state;
+    const { currPass, newPass, confirm } = this.state;
     if (newPass !== confirm) {
-      this.setState({ error: 'New passwords do not match. Please double check.' });
-      swal('Error', error, 'error');
+      swal('Error', 'New passwords do not match. Please double check.', 'error');
     } else {
       Accounts.changePassword(currPass, newPass, (err) => {
-          if (err) {
-            this.setState({ error: err.reason });
-            swal('Error', err.message, 'error');
-          } else {
-            swal('Success', 'Password changed successfully', 'success');
-          }
-        });
+        if (err) {
+          swal('Error', 'Current password is incorrect. Please try again.', 'error');
+        } else {
+          swal('Success', 'Password changed successfully', 'success');
+        }
+      });
 
     }
   }
 
-  /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
+  /* If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
     return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
   }
 
-  /** Render the form. Use Uniforms: https://github.com/vazco/uniforms */
+  /* Render the form. Use Uniforms: https://github.com/vazco/uniforms */
   renderPage() {
     const pageStyle = {
       marginLeft: '20em',
@@ -65,54 +62,55 @@ class ChangePassword extends React.Component {
           height: '100%',
           marginTop: '-10px',
         }}>
-          <NavBarHome/>
-          <Container style={pageStyle}>
-            <Header inverted as="h2" textAlign="center">Change Password</Header>
-            <Image src='images/HEI-WAVE-LOGO.png' centered size='small' style={{
-              paddingBottom: '50px',
-            }}/>
-            <Card style={{ margin: 'auto' }}>
-
-              <Form onSubmit={data => {
-                // eslint-disable-next-line no-undef,no-alert
-                if (window.confirm('Are you sure you wish to save your changes?')) this.submit(data);
-              }}>
-                <Form.Input
-                    label="Current Password"
-                    icon="user"
-                    iconPosition="left"
-                    name="currPass"
-                    type="password"
-                    placeholder="Enter your current password"
-                    onChange={this.handleChange}
-                />
-                <Form.Input
-                    label="New Password"
-                    icon="user"
-                    iconPosition="left"
-                    name="newPass"
-                    type="password"
-                    placeholder="Enter a new password"
-                    onChange={this.handleChange}
-                />
-                <Form.Input
-                    label="Confirm New Password"
-                    icon="user"
-                    iconPosition="left"
-                    name="confirm"
-                    type="password"
-                    placeholder="Confirm new password"
-                    onChange={this.handleChange}
-                />
-
-                <Form.Button content={'Submit'}>
-
-                </Form.Button>
-
-              </Form>
-
-            </Card>
-          </Container>
+          <Grid columns={2}>
+            <Grid.Column width={5}>
+              <NavBarHome/>
+            </Grid.Column>
+            <Grid.Column width={8}>
+              <Container style={pageStyle}>
+                <Header inverted as="h2" textAlign="center">CHANGE PASSWORD</Header>
+                <Image src='images/HEI-WAVE-LOGO.png' centered size='small' style={{
+                  paddingBottom: '50px',
+                }}/>
+                <Segment className='viewProfile' stacked inverted>
+                  <Form inverted onSubmit={data => {
+                    // eslint-disable-next-line no-undef,no-alert
+                    if (window.confirm('Are you sure you wish to save your changes?')) this.submit(data);
+                  }}>
+                    <Form.Input
+                        label="Current Password"
+                        icon="eye slash"
+                        iconPosition="left"
+                        name="currPass"
+                        type="password"
+                        placeholder="Enter your current password"
+                        onChange={this.handleChange}
+                    />
+                    <Form.Input
+                        label="New Password"
+                        icon="eye slash"
+                        iconPosition="left"
+                        name="newPass"
+                        type="password"
+                        placeholder="Enter a new password"
+                        onChange={this.handleChange}
+                    />
+                    <Form.Input
+                        label="Confirm New Password"
+                        icon="lock"
+                        iconPosition="left"
+                        name="confirm"
+                        type="password"
+                        placeholder="Confirm new password"
+                        onChange={this.handleChange}
+                    />
+                    <Form.Button color={'blue'} content={'Submit'}>
+                    </Form.Button>
+                  </Form>
+                </Segment>
+              </Container>
+            </Grid.Column>
+          </Grid>
         </div>
     );
   }
