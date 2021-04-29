b4e81e0504f39566307fb6bed12ec915453f8080
christianjensenv <cjensen6@hawaii.edu>
2021-04-28 20:37:18 -1000

    test1

--- app/imports/ui/pages/Signout.jsx
+++ app/imports/ui/pages/Signout.jsx
@@ -1,55 +1,41 @@
 import React from 'react';
-import { Button, Header, Icon, Modal } from 'semantic-ui-react';
 import { Meteor } from 'meteor/meteor';
+import swal from 'sweetalert';
 import Landing from './landing/Landing';
 
 /** After the user clicks the "Signout" link in the NavBar, log them out and display this page. */
-
-function ModalExampleBasic() {
-  const [open, setOpen] = React.useState(false);
-  const [signout] = React.useState(false);
-  return (
-      <Modal
-          basic
-          onClose={() => setOpen(false)}
-          onOpen={() => setOpen(true)}
-          open={open}
-          size='small'
-          trigger={<Button>Basic Modal</Button>}
-      >
-        <Header icon>
-          <Icon name='archive'/>
-          Archive Old Messages
-        </Header>
-        <Modal.Content>
-          <p>
-            Would you like to sign out?
-          </p>
-        </Modal.Content>
-        <Modal.Actions>
-          <Button basic color='red' inverted onClick={() => setOpen(false)}>
-            <Icon name='remove'/> Cancel
-          </Button>
-          <Button color='green' inverted onClick={() => setOpen(false)}>
-            <Icon name='checkmark'/> Yes
-          </Button>
-        </Modal.Actions>
-      </Modal>
-  );
-}
-
 export default class Signout extends React.Component {
-
   render() {
-    ModalExampleBasic();
-    Meteor.logout(function (event) {
-      event.
+    swal({
+      text: 'Are you sure you want to sign out?',
+      buttons: {
+        confirm: {
+          text: 'Yes',
+          value: true,
+          visible: true,
+        },
+        cancel: {
+          text: 'Cancel',
+          value: false,
+          visible: true,
+        },
+      },
+    }).then((val) => {
+      if (val) {
+        Meteor.logout();
+      }
     });
+    if (Meteor.user() === null) {
       return (
           <div id="signout-page">
             <Landing/>
           </div>
       );
     }
+    return (
+        <div id="home">
+          <home/>
+        </div>
+    );
   }
 }
